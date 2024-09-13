de633e651c506fd97fd7d6b210229f92b5b3555c
sulemanlord <sulaimanmind862@gmail.com>
2024-09-13 15:47:10 +0600

    save

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {StatusBar} from 'react-native'; // Импортируем StatusBar
 import Login from './Auth/Login';
 import Verification from './Auth/Verification';
 import MainNavigation from '../MainStackNavigation';
@@ -23,16 +24,13 @@ export default function Main() {
     const loadUser = async () => {
       const savedUser = await getUserFromStorage();
       const isLoggedIn = await checkLoginDate();
-
       if (savedUser && isLoggedIn) {
         dispatch(authActions.saveUser(savedUser));
       } else {
         dispatch(authActions.logout());
       }
-
       setLoading(false);
     };
-
     loadUser();
   }, [dispatch]);
 
@@ -41,34 +39,41 @@ export default function Main() {
   }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={user ? 'MainNavigation' : 'Login'}>
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="Login"
-          component={Login}
-        />
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="SignUp"
-          component={SignUp}
-        />
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="Verification"
-          component={Verification}
-        />
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="MainNavigation"
-          component={MainNavigation}
-        />
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="OfficesLog"
-          component={OfficesLog}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={user ? 'MainNavigation' : 'Login'}>
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="Login"
+            component={Login}
+          />
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="SignUp"
+            component={SignUp}
+          />
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="Verification"
+            component={Verification}
+          />
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="MainNavigation"
+            component={MainNavigation}
+          />
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="OfficesLog"
+            component={OfficesLog}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
